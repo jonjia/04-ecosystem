28082791433e280cedad2099f0941fc07d8fe2c6
jonjia <jiawenhuiwar3@163.com>
2024-06-06 16:27:41 +0800

    feat: bytes

---
+++ examples/bytes.rs
@@ -0,0 +1,17 @@
+use anyhow::Result;
+use bytes::{BufMut, BytesMut};
+
+fn main() -> Result<()> {
+    let mut buf = BytesMut::with_capacity(1024);
+    buf.extend_from_slice(b"hello world\n");
+    buf.put(&b"goodbye world"[..]);
+    buf.put_i64(0xdeadbeef);
+    println!("{:?}", buf);
+    let a = buf.split();
+    let mut b = a.freeze();
+    // let pos = b.binary_search(&0).unwrap();
+    let c = b.split_to(12);
+    println!("{:?}", c);
+    println!("{:?}", b);
+    Ok(())
+}
